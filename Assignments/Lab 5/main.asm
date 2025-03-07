2a70d0b1605a9257095bcf955508633b24077424
Ryan Chan <chanrya@sonoma.edu>
2025-03-07 00:12:00 -0800

    Add files via upload

---
+++ Assignments/Lab 5/main.asm
@@ -0,0 +1,84 @@
+;---------------------
+; Title: Waveform Generator with Delay
+;---------------------
+; Program Details:
+; The purpose of this program is to demonstrate how to call a delay function. 
+
+; Inputs: Inner_loop ,Outer_loop 
+; Outputs: PORTD
+; Date: Mar 4, 2025
+; File Dependencies / Libraries: None 
+; Compiler: xc8, 3.0
+; Author: Ryan Chan
+; Versions:
+;       V1.0
+; Useful links: 
+;       Datasheet: https://ww1.microchip.com/downloads/en/DeviceDoc/PIC18(L)F26-27-45-46-47-55-56-57K42-Data-Sheet-40001919G.pdf 
+;       PIC18F Instruction Sets: https://onlinelibrary.wiley.com/doi/pdf/10.1002/9781119448457.app4 
+;       List of Instrcutions: http://143.110.227.210/faridfarahmand/sonoma/courses/es310/resources/20140217124422790.pdf 
+
+
+;---------------------
+; Initialization - make sure the path is correct
+;---------------------
+#include "myConfigFile.inc"
+;#include "C:\Users\student\Documents\myMPLABXProjects\ProjectFirstAssemblyMPLAB\FirstAssemblyMPLAB.X\AssemblyConfig.inc"
+
+#include <xc.inc>
+
+;---------------------
+; Program Inputs
+;---------------------
+;Inner_loop  equ 5 // in decimal
+;Outer_loop  equ 5
+ 
+;---------------------
+; Program Constants
+;---------------------
+REG10   equ     10h   // in HEX
+REG11   equ     11h
+REG01   equ     1h
+
+;---------------------
+; Definitions
+;---------------------
+#define SWITCH    LATD,2  
+#define LED0      PORTD,0
+#define LED1	  PORTD,1
+
+;---------------------
+; Main Program
+;---------------------
+    PSECT absdata,abs,ovrld        ; Do not change
+    
+    ORG          0                ;Reset vector
+    GOTO        _start1
+
+    ORG          0020H           ; Begin assembly at 0020H
+
+_start1:
+    MOVLW       0b11111100
+    MOVWF       TRISD,0
+    MOVLW       0b01		 ; Set PORTD0 and PORTD1 to be different
+    MOVWF       REG01,0
+
+_onoff:
+    MOVFF       REG01,PORTD
+    MOVLW       5
+    MOVWF       REG10
+    MOVLW       5
+    MOVWF       REG11
+
+_loop1:
+    DECF        REG10,1
+    BNZ         _loop1
+    DECF        REG11,1
+    BNZ         _loop1
+    
+    COMF        REG01,1 // Negate the register 
+    BRA         _onoff
+    
+;    MOVLW	45
+;    MOVLW	2DH
+;    MOVLW	0b00101101
+END
