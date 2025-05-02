9f066c0c3dee54ab257eae3e2b75300e9429b404
Ryan Chan <chanrya@sonoma.edu>
2025-05-02 10:00:35 -0700

    Add files via upload

---
+++ Assignments/Assignment 8/lcd.h
@@ -0,0 +1,95 @@
+#ifndef LCD_H
+#define LCD_H
+
+#include <xc.h> // must have this
+
+#define _XTAL_FREQ 4000000                 // Fosc  frequency for _delay()  library
+#define FCY    _XTAL_FREQ/4
+
+#define RS LATC5                   /* PORTC 5 pin is used for Register Select */
+#define EN LATC6                   /* PORTC 6 pin is used for Enable */
+#define ldata LATD                 /* PORTD is used for transmitting data to LCD */
+
+#define LCD_Port TRISD              
+#define LCD_Control TRISC
+
+void LCD_Init();
+void LCD_Command(char );
+void LCD_Char(char x);
+void LCD_String(const char *);
+void LCD_String_xy(char ,char ,const char*);
+void LCD_Clear();
+void MSdelay(unsigned int );
+
+/****************************Functions********************************/
+void LCD_Init()
+{
+    MSdelay(15);           /* 15ms,16x2 LCD Power on delay */
+    LCD_Port = 0x00;       /* Set PORTB as output PORT for LCD data(D0-D7) pins */
+    LCD_Control = 0x00;    /* Set PORTD as output PORT LCD Control(RS,EN) Pins */
+    LCD_Command(0x01);     /* clear display screen */
+    LCD_Command(0x38);     /* uses 2 line and initialize 5*7 matrix of LCD */
+    LCD_Command(0x0c);     /* display on cursor off */
+    LCD_Command(0x06);     /* increment cursor (shift cursor to right) */
+}
+
+void LCD_Clear()
+{
+    LCD_Command(0x01);     /* clear display screen */
+}
+
+void LCD_Command(char cmd )
+{
+    ldata= cmd;            /* Send data to PORT as a command for LCD */   
+    RS = 0;                /* Command Register is selected */
+    EN = 1;                /* High-to-Low pulse on Enable pin to latch data */ 
+    NOP();
+    EN = 0;
+    MSdelay(3); 
+}
+
+void LCD_Char(char dat)
+{
+    ldata= dat;            /* Send data to LCD */  
+    RS = 1;                /* Data Register is selected */
+    EN=1;                  /* High-to-Low pulse on Enable pin to latch data */   
+    NOP();
+    EN=0;
+    MSdelay(1);
+}
+
+
+void LCD_String(const char *msg)
+{
+    while((*msg)!=0)
+    {       
+      LCD_Char(*msg);
+      msg++;    
+    }
+}
+
+void LCD_String_xy(char row,char pos,const char *msg)
+{
+    char location=0;
+    if(row<=1)
+    {
+        location=(0x80) | ((pos) & 0x0f); /*Print message on 1st row and desired location*/
+        LCD_Command(location);
+    }
+    else
+    {
+        location=(0xC0) | ((pos) & 0x0f); /*Print message on 2nd row and desired location*/
+        LCD_Command(location);    
+    }  
+    LCD_String(msg);
+
+}
+/*********************************Delay Function********************************/
+void MSdelay(unsigned int val)
+{
+    unsigned int i,j;
+    for(i=0;i<val;i++)
+        for(j=0;j<165;j++);      /*This count Provide delay of 1 ms for 8MHz Frequency */
+}
+
+#endif
